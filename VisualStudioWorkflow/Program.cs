5d11926fc7275d203f3cb5d006376dea5092980d
giakhanht <giakhanht.cs@gmail.com>
2024-05-05 12:35:54 +0200

    Learn the differences between Pull and Fetch

--- VisualStudioWorkflow/Program.cs
+++ VisualStudioWorkflow/Program.cs
@@ -13,6 +13,9 @@ namespace VisualStudioWorkflow
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Modification to Pull in VS!");
+            Console.WriteLine("Another modification to Fetch in VS!!");
+            //Fetch stores additional commits not available in the current branch in the local repository, but does not merge them, while Pull does.
+            //Verdict: Fetch is useful for simply keeping your repo up-to-date, but you have to merge afterwards.
             Console.Read();
         }
     }
